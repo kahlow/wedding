6c1e8918dfb8e09f37c17094005f0295f6bc3034
kahlow <tkahlow@gmail.com>
2014-11-14 18:39:33 -0500

    instagram

--- js/app.js
+++ js/app.js
@@ -73,7 +73,8 @@ $(document).ready(function(){
     var feed = new Instafeed({
         get: 'tagged',
         tagName: 'kahlow2014',
-        clientId: 'c8891fa90e64440989271dc1b18c53b9'
+        clientId: 'c8891fa90e64440989271dc1b18c53b9',
+        resolution: 'standard_resolution'
     });
     feed.run();
 });
